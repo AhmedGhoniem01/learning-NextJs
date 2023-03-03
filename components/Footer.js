5601c5d86549672c7efb1a71e96cf7a2b3d2af16
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-03-03 16:40:37 +0200

    lesson 3

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <footer>
+            <div className="logo">
+                <h1>Created by a ninja Copyright 2023</h1>
+            </div>
+      </footer>
+
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
-// import styles from '@/styles/Home.module.css'
-
 //In next, the pages file include the pages to be routed to
 //each page file name is also the name of its route
 //index is the main route so (index.js => '/' route)
- 
+//Creating a components folder is necessary to crete reusable html components
+//the components can be imported and reused in a page file such as shown and in a similar way as react.
+//Notice the component tag and don't forget to imprt it
+
+import Footer from "@/components/Footer"
+import Navbar from "@/components/Navbar"
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p>    </>
-  )
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi impedit suscipit architecto, odio inventore nostrum non neque dicta. Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem nobis odit.</p> 
+      <Footer />
+    </>
+  );
 }
