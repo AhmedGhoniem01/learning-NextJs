cec37c3dfe1515ed5999c2bee49608a5d0ce1d5f
AhmedGhoniem01 <ahmed.ghoniem01@gmail.com>
2023-03-03 18:02:09 +0200

    lesson-8

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,27 @@
 //404.js => the custom error page when no urlis matched
 //you can make custom css file and import it or you can just embed the css styles int he global file
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function NotFound(){
+    //the router is used to navigte to different urls
+    const router = useRouter();
+    //the useEffect is used here to run the internal functions once the coponent/page is first renderd
+    useEffect(() => {
+        setTimeout(() => {
+            //router.push => diectly route to the following url
+            //router.go(-1) => return back
+            //router.go(1) => goes forward
+            router.push('/')
+        }, 5000);
+    }, [])
+
     return(
         <div className="not-found">
-            <h1>Page not found</h1>
+            <h1>404 Page not found</h1>
             <h3>Check you navigate to a correct link...</h3>
-            <Link href='/'><p>Return to Home-Page</p></Link>
+            <Link href='/'><p>Returning to Home-Page...</p></Link>
         </div>
     );
 }
